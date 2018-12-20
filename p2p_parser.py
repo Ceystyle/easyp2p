19db8ec777f86ddcdc6706b2d35c2b62717fdcb5
Niko Sandschneider <nsandschn@gmx.de>
2018-12-20 12:51:21 +0100

    Introduce global variables for naming cashflow types

--- p2p_parser.py
+++ p2p_parser.py
@@ -4,6 +4,17 @@ import locale
 import pandas as pd
 import xlrd
 
+interest_payment = 'Zinszahlungen'
+buyback_interest_payment = 'Zinszahlungen aus Rückkäufen'
+buyback_payment = 'Rückkäufe'
+investment_payment = 'Investitionen'
+redemption_payment = 'Tilgungszahlungen'
+late_fee_payment = 'Verzugsgebühren'
+incoming_payment = 'Einzahlungen'
+outgoing_payment = 'Auszahlungen'
+default_payment = 'Ausfälle'
+start_balance_name = 'Startguthaben'
+end_balance_name = 'Endsaldo'
 
 def read_excel(p2p_name,  filename):
     try:
@@ -33,19 +44,21 @@ def bondora():
     df.rename_axis('Datum', inplace=True)
     df.rename(
         columns={
-            'Eingesetztes Kapital (netto)': 'Einzahlungen',
-            'Erhaltene Zinsen - gesamt': 'Zinszahlungen',
-            'Erhaltener Kapitalbetrag - gesamt': 'Tilgungszahlungen',
-            'Investitionen (netto)': 'Investitionen',
+            'Eingesetztes Kapital (netto)': incoming_payment,
+            'Erhaltene Zinsen - gesamt': interest_payment,
+            'Erhaltener Kapitalbetrag - gesamt': redemption_payment,
+            'Investitionen (netto)': investment_payment,
         },
         inplace=True
     )
     df.rename(
         columns={
-            'Darlehensbetrag und erhaltene Zinsen - insgesamt': 'Gesamtzahlungen',
+            'Darlehensbetrag und erhaltene Zinsen - insgesamt':
+                'Gesamtzahlungen',
             'Geplante Zinsen - gesamt': 'Geplante Zinszahlungen',
             'Geplanter Kapitalbetrag - gesamt': 'Geplante Tilgungszahlungen',
-            'Kapitalbetrag und geplante Zinsen - gesamt': 'Geplante Gesamtzahlungen'
+            'Kapitalbetrag und geplante Zinsen - gesamt':
+                'Geplante Gesamtzahlungen'
         },
         inplace=True
     )
@@ -53,7 +66,8 @@ def bondora():
 
     df['Währung'] = 'EUR'
     df['Plattform'] = 'Bondora'
-    df['Ausfälle'] = df['Tilgungszahlungen'] - df['Geplante Tilgungszahlungen']
+    df[default_payment] = (df['Tilgungszahlungen']
+                           - df['Geplante Tilgungszahlungen'])
 
     df.reset_index(level=0, inplace=True)
     # TODO: make sure locale is installed
@@ -74,17 +88,17 @@ def mintos():
         return None
 
     mintos_dict = dict()
-    mintos_dict['Interest income'] = 'Zinszahlungen'
-    mintos_dict['Interest income on rebuy'] = 'Zinszahlungen aus Rückkäufen'
-    mintos_dict['Delayed interest income on rebuy'] = 'Zinszahlungen aus Rückkäufen'
-    mintos_dict['Investment principal rebuy'] = 'Rückkäufe'
-    mintos_dict['Investment principal increase'] = 'Investitionen'
-    mintos_dict['Investment principal repayment'] = 'Tilgungszahlungen'
-    mintos_dict['Late payment fee income'] = 'Verzugsgebühren'
-    mintos_dict['Incoming client payment'] = 'Einzahlungen'
+    mintos_dict['Interest income'] = interest_payment
+    mintos_dict['Interest income on rebuy'] = buyback_interest_payment
+    mintos_dict['Delayed interest income on rebuy'] = buyback_interest_payment
+    mintos_dict['Investment principal rebuy'] = buyback_payment
+    mintos_dict['Investment principal increase'] = investment_payment
+    mintos_dict['Investment principal repayment'] = redemption_payment
+    mintos_dict['Late payment fee income'] = late_fee_payment
+    mintos_dict['Incoming client payment'] = incoming_payment
     # Treat bonus/cashback payments as normal interest payments
-    mintos_dict['Cashback bonus'] = 'Zinszahlungen'
-    mintos_dict['Reversed incoming client payment'] = 'Auszahlungen'
+    mintos_dict['Cashback bonus'] = interest_payment
+    mintos_dict['Reversed incoming client payment'] = outgoing_payment
 
     df.rename(columns={'Date': 'Datum',  'Currency': 'Währung'},  inplace=True)
     df['Datum'] = pd.to_datetime(df['Datum'])
@@ -119,11 +133,11 @@ def robocash():
         return None
 
     robocash_dict = dict()
-    robocash_dict['Zinsenzahlung'] = 'Zinszahlungen'
-    robocash_dict['Darlehenskauf'] = 'Investitionen'
-    robocash_dict['Kreditrückzahlung'] = 'Tilgungszahlungen'
-    robocash_dict['Die Geldauszahlung'] = 'Auszahlungen'
-    robocash_dict['Geldeinzahlung'] = 'Einzahlungen'
+    robocash_dict['Zinsenzahlung'] = interest_payment
+    robocash_dict['Darlehenskauf'] = investment_payment
+    robocash_dict['Kreditrückzahlung'] = redemption_payment
+    robocash_dict['Die Geldauszahlung'] = outgoing_payment
+    robocash_dict['Geldeinzahlung'] = incoming_payment
 
     df = df[df.Operation != 'Die Geldauszahlung aus dem Portfolio']
     df = df[df.Operation != 'Portfolio auffüllen']
@@ -154,12 +168,12 @@ def swaper():
         return None
 
     swaper_dict = dict()
-    swaper_dict['REPAYMENT_INTEREST'] = 'Zinszahlungen'
-    swaper_dict['EXTENSION_INTEREST'] = 'Zinszahlungen'
-    swaper_dict['INVESTMENT'] = 'Investitionen'
-    swaper_dict['REPAYMENT_PRINCIPAL'] = 'Tilgungszahlungen'
-    swaper_dict['BUYBACK_INTEREST'] = 'Zinszahlungen aus Rückkäufen'
-    swaper_dict['BUYBACK_PRINCIPAL'] = 'Rückkäufe'
+    swaper_dict['REPAYMENT_INTEREST'] = interest_payment
+    swaper_dict['EXTENSION_INTEREST'] = interest_payment
+    swaper_dict['INVESTMENT'] = investment_payment
+    swaper_dict['REPAYMENT_PRINCIPAL'] = redemption_payment
+    swaper_dict['BUYBACK_INTEREST'] = buyback_interest_payment
+    swaper_dict['BUYBACK_PRINCIPAL'] = buyback_payment
 
     df.rename(columns={'Booking date': 'Datum'},  inplace=True)
     df['Datum'] = df['Datum'].dt.strftime('%d.%m.%Y')
@@ -187,11 +201,12 @@ def peerberry():
         return None
 
     peerberry_dict = dict()
-    peerberry_dict['Amount of interest payment received'] = 'Zinszahlungen'
+    peerberry_dict['Amount of interest payment received'] = interest_payment
     peerberry_dict['Investment'] = 'Investitionen'
-    peerberry_dict['Amount of principal payment received'] = 'Tilgungszahlungen'
+    peerberry_dict['Amount of principal payment received'] = redemption_payment
 
-    df.rename(columns={'Date': 'Datum',  'Currency Id': 'Währung'},  inplace=True)
+    df.rename(
+        columns={'Date': 'Datum', 'Currency Id': 'Währung'}, inplace=True)
     df['Datum'] = pd.to_datetime(df['Datum'],  format='%Y-%m-%d')
     df['Datum'] = df['Datum'].dt.strftime('%d.%m.%Y')
     df['Cashflow-Typ'] = df['Type'].map(peerberry_dict)
@@ -217,13 +232,13 @@ def estateguru():
         return None
 
     estateguru_dict = dict()
-    estateguru_dict['Zins'] = 'Zinszahlungen'
+    estateguru_dict['Zins'] = interest_payment
     # Treat bonus payments as normal interest payments
-    estateguru_dict['Bonus'] = 'Zinszahlungen'
-    estateguru_dict['Investition  (Auto Investieren)'] = 'Investitionen'
-    estateguru_dict['Hauptbetrag'] = 'Tilgungszahlungen'
-    estateguru_dict['Einzahlung  (Banktransfer)'] = 'Einzahlungen'
-    estateguru_dict['Entschädigung'] = 'Verzugsgebühren'
+    estateguru_dict['Bonus'] = interest_payment
+    estateguru_dict['Investition  (Auto Investieren)'] = investment_payment
+    estateguru_dict['Hauptbetrag'] = redemption_payment
+    estateguru_dict['Einzahlung  (Banktransfer)'] = incoming_payment
+    estateguru_dict['Entschädigung'] = late_fee_payment
 
     df = df[:-1]  # Drop last line which only contains a summary
     df.rename(
@@ -261,33 +276,33 @@ def iuvo():
     if df is None:
         return None
 
-    df['Zinszahlungen'] = 0
-    df['Tilgungszahlungen'] = 0
+    df[interest_payment] = 0
+    df[redemption_payment] = 0
     # Date column will raise an error which can be ignored:
     df = df.astype('float64', errors='ignore')
 
     interest_types = ['Zins erhalten', 'Vorzeitige Zinstilgung']
     for it in interest_types:
         if it in df.columns:
-            df['Zinszahlungen'] += df[it]
+            df[interest_payment] += df[it]
             del df[it]
 
     redemption_types = [
         'Vorzeitige Kreditbetragtilgung', 'Kreditbetrag erhalten']
     for rt in redemption_types:
         if rt in df.columns:
-            df['Tilgungszahlungen'] += df[rt]
+            df[redemption_payment] += df[rt]
             del df[rt]
 
     df.rename(
         columns={
-            'Anfangsbestand': 'Startguthaben',
-            'Automatische Kapitalanlage auf dem Primärmarkt': 'Investitionen',
-            'Endbestand': 'Endsaldo',
-            'Kreditbetrag bei Rückkauf erhalten': 'Rückkäufe',
-            'Verzugsstrafen erhalten': 'Verzugsgebühren'
-        }, inplace=True
-    )
+            'Anfangsbestand': start_balance_name,
+            'Automatische Kapitalanlage auf dem Primärmarkt':
+                investment_payment,
+            'Endbestand': end_balance_name,
+            'Kreditbetrag bei Rückkauf erhalten': buyback_payment,
+            'Verzugsstrafen erhalten': late_fee_payment
+        }, inplace=True)
     df['Datum'] = pd.to_datetime(df['Datum'], format='%d.%m.%Y')
     df['Datum'] = df['Datum'].dt.strftime('%d.%m.%Y')
     df['Plattform'] = 'Iuvo'
@@ -308,12 +323,12 @@ def grupeer():
         return None
 
     grupeer_dict = dict()
-    grupeer_dict['Interest'] = 'Zinszahlungen'
-    grupeer_dict['Investment'] = 'Investitionen'
-    grupeer_dict['Deposit'] = 'Einzahlungen'
+    grupeer_dict['Interest'] = interest_payment
+    grupeer_dict['Investment'] = investment_payment
+    grupeer_dict['Deposit'] = incoming_payment
     # Treat cashback as interest payment:
-    grupeer_dict['Cashback'] = 'Zinszahlungen'
-    grupeer_dict['Principal'] = 'Tilgungszahlungen'
+    grupeer_dict['Cashback'] = interest_payment
+    grupeer_dict['Principal'] = redemption_payment
 
     df.rename(columns={'Date': 'Datum'},  inplace=True)
     df['Datum'] = pd.to_datetime(df['Datum'],  format="%d.%m.%Y")
@@ -344,10 +359,12 @@ def dofinance():
         return None
 
     dofinance_dict = dict()
-    dofinance_dict['Verdienter Gewinn'] = 'Zinszahlungen'
-    dofinance_dict['Auszahlung auf Bankkonto'] = 'Auszahlungen'
-    dofinance_dict['Abgeschlossene Investition\nRate: 12% Typ: automatisch'] = 'Tilgungszahlungen'
-    dofinance_dict['Anlage\nRate: 12% Typ: automatisch'] = 'Investitionen'
+    dofinance_dict['Verdienter Gewinn'] = interest_payment
+    dofinance_dict['Auszahlung auf Bankkonto'] = outgoing_payment
+    dofinance_dict[
+        'Abgeschlossene Investition\nRate: 12% Typ: automatisch'] = \
+        redemption_payment
+    dofinance_dict['Anlage\nRate: 12% Typ: automatisch'] = investment_payment
 
     df = df[:-2]  # drop the last two rows
     df.rename(columns={'Bearbeitungsdatum': 'Datum'},  inplace=True)
