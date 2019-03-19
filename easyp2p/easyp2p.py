5aa5575ea3282442ed0a536955433d8b29bb5d5f
Niko Sandschneider <nsandschn@gmx.de>
2019-03-19 11:18:11 +0100

    Create a main entry point for easyp2p

--- easyp2p/easyp2p.py
+++ easyp2p/easyp2p.py
@@ -2,19 +2,27 @@
 # Copyright 2018-19 Niko Sandschneider
 
 """
-    easyP2P gets investment results from various P2P platforms.
+easyp2p.
+
+Package for downloading and presenting investment results for several P2P
+lending platforms in a unified way.
+
 """
+import sys
+from PyQt5 import QtWidgets
+from easyp2p.ui.main_window import MainWindow
+
+name = "easyp2p"
 __author__ = 'Niko Sandschneider'
 __copyright__ = 'Copyright (C) 2018-19 Niko Sandschneider'
 __license__ = 'MIT'
-__version__ = '0.1'
+__version__ = '0.0.1'
 
-import sys
-from PyQt5 import QtWidgets
-from ui.main_window import MainWindow
-
-if __name__ == "__main__":
+def main():
     app = QtWidgets.QApplication(sys.argv)
     ui = MainWindow()
     ui.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
