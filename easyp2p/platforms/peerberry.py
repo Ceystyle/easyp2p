af841b22827142b3473a42f1768d600d77cf080d
Niko Sandschneider <nsandschn@gmx.de>
2019-03-28 12:02:17 +0100

    Introduce PeerBerry class

--- easyp2p/platforms/peerberry.py
+++ easyp2p/platforms/peerberry.py
@@ -1,7 +1,7 @@
 # Copyright 2018-19 Niko Sandschneider
 
 """
-Download and parse Peerberry statement.
+Download and parse PeerBerry statement.
 
 .. moduleauthor:: Niko Sandschneider <nsandschn@gmx.de>
 
@@ -20,124 +20,133 @@ from easyp2p.p2p_parser import P2PParser
 from easyp2p.p2p_platform import P2PPlatform
 
 
-def download_statement(
-        date_range: Tuple[date, date],
-        credentials: Tuple[str, str]) -> None:
-    """
-    Generate and download the PeerBerry account statement for given date range.
+class PeerBerry:
 
-    Args:
-        date_range (tuple(date, date)): date range
-            (start_date, end_date) for which the account statements must
-            be generated.
-        credentials (tuple[str, str]): (username, password) for PeerBerry
+    def __init__(self, date_range: Tuple[date, date]) -> None:
+        """
+        Constructor of PeerBerry class.
 
-    """
-    urls = {
-        'login': 'https://peerberry.com/de/login',
-        'statement': 'https://peerberry.com/de/statement'}
-    xpaths = {
-        'cookie_policy': '//*[@id="app"]/div/div/div/div[4]/div/div/div[1]',
-        'download_btn': ('//*[@id="app"]/div/div/div/div[2]/div/div[2]/div[3]/'
-                         'div[2]/div'),
-        'logout_btn': ('//*[@id="app"]/div/div/div/div[1]/div[1]/div/div/'
-                       'div[2]/div'),
-        'start_balance': ('/html/body/div[1]/div/div/div/div[2]/div/div[2]/'
-                          'div[2]/div/div/div[1]'),
-        'statement_btn': ('/html/body/div[1]/div/div/div/div[2]/div/div[2]/'
-                          'div[1]/div/div[2]/div/div[2]/div/span')}
+        Args:
+            date_range: date range (start_date, end_date) for which the account
+                statements must be generated
 
-    with P2PPlatform(
-            'PeerBerry', urls, EC.title_contains('Einloggen'),
-            logout_locator=(By.XPATH, xpaths['logout_btn'])) as peerberry:
+        """
+        self.name = 'PeerBerry'
+        self.date_range = date_range
 
-        peerberry.log_into_page(
-            'email', 'password', credentials,
-            EC.element_to_be_clickable((By.LINK_TEXT, 'Kontoauszug')))
+    def download_statement(self, credentials: Tuple[str, str]) -> None:
+        """
+        Generate and download the PeerBerry account statement.
 
-        peerberry.open_account_statement_page(
-            'Kontoauszug', (By.NAME, 'startDate'))
+        Args:
+            credentials (tuple[str, str]): (username, password) for PeerBerry
 
-        # Close the cookie policy, if present
-        try:
-            peerberry.driver.find_element_by_xpath(
-                xpaths['cookie_policy']).click()
-        except NoSuchElementException:
-            pass
+        """
+        urls = {
+            'login': 'https://peerberry.com/de/login',
+            'statement': 'https://peerberry.com/de/statement'}
+        xpaths = {
+            'cookie_policy': '//*[@id="app"]/div/div/div/div[4]/div/div/div[1]',
+            'download_btn': ('//*[@id="app"]/div/div/div/div[2]/div/div[2]/'
+                             'div[3]/div[2]/div'),
+            'logout_btn': ('//*[@id="app"]/div/div/div/div[1]/div[1]/div/div/'
+                           'div[2]/div'),
+            'start_balance': ('/html/body/div[1]/div/div/div/div[2]/div/div[2]/'
+                              'div[2]/div/div/div[1]'),
+            'statement_btn': ('/html/body/div[1]/div/div/div/div[2]/div/div[2]/'
+                              'div[1]/div/div[2]/div/div[2]/div/span')}
 
-        # Create account statement for given date range
-        default_dates = (date.today(), date.today())
-        arrows = {'arrow_tag': 'th',
-                  'left_arrow_class': 'rdtPrev',
-                  'right_arrow_class': 'rdtNext'}
-        calendar_locator = ((By.NAME, 'startDate'), (By.NAME, 'endDate'))
-        days_table = {'class_name': 'rdtDays',
-                      'current_day_id': 'rdtDay',
-                      'id_from_calendar': False,
-                      'table_id': 'class'}
+        with P2PPlatform(
+                'PeerBerry', urls, EC.title_contains('Einloggen'),
+                logout_locator=(By.XPATH, xpaths['logout_btn'])) as peerberry:
 
-        peerberry.generate_statement_calendar(
-            date_range, default_dates, arrows, days_table, calendar_locator)
+            self.statement_file_name = peerberry.set_statement_file_name(
+                self.date_range, 'csv')
 
-        # After setting the dates, the statement button needs to be clicked in
-        # order to actually generate the statement
-        try:
-            peerberry.driver.find_element_by_xpath(
-                xpaths['statement_btn']).click()
-            peerberry.wdwait(
-                EC.text_to_be_present_in_element(
-                    ((By.XPATH, xpaths['start_balance'])),
-                    'Eröffnungssaldo '+str(date_range[0]).format('%Y-%m-%d')))
-        except NoSuchElementException:
-            raise RuntimeError('Generierung des PeerBerry-Kontoauszugs konnte '
-                               'nicht gestartet werden.')
-        except TimeoutException:
-            raise RuntimeError('Generierung des PeerBerry-Kontoauszugs hat zu '
-                               'lange gedauert.')
+            peerberry.log_into_page(
+                'email', 'password', credentials,
+                EC.element_to_be_clickable((By.LINK_TEXT, 'Kontoauszug')))
 
-        peerberry.download_statement(
-            'transactions*.csv', (By.XPATH, xpaths['download_btn']),
-            actions='move_to_element')
+            peerberry.open_account_statement_page(
+                'Kontoauszug', (By.NAME, 'startDate'))
 
+            # Close the cookie policy, if present
+            try:
+                peerberry.driver.find_element_by_xpath(
+                    xpaths['cookie_policy']).click()
+            except NoSuchElementException:
+                pass
 
-def parse_statement(
-        date_range: Tuple[date, date],
-        input_file: str = 'p2p_downloads/peerberry_statement.csv') \
-        -> Tuple[pd.DataFrame, str]:
-    """
-    Parser for Peerberry.
+            # Create account statement for given date range
+            default_dates = (date.today(), date.today())
+            arrows = {'arrow_tag': 'th',
+                      'left_arrow_class': 'rdtPrev',
+                      'right_arrow_class': 'rdtNext'}
+            calendar_locator = ((By.NAME, 'startDate'), (By.NAME, 'endDate'))
+            days_table = {'class_name': 'rdtDays',
+                          'current_day_id': 'rdtDay',
+                          'id_from_calendar': False,
+                          'table_id': 'class'}
 
-    Args:
-        date_range (tuple(date, date)): date range
-            (start_date, end_date) for which the investment results must be
-            shown.
+            peerberry.generate_statement_calendar(
+                self.date_range, default_dates, arrows, days_table,
+                calendar_locator)
 
-    Keyword Args:
-        input_file (str): file name including path of the account statement
-            downloaded from the PeerBerry web site
+            # After setting the dates, the statement button needs to be clicked in
+            # order to actually generate the statement
+            try:
+                peerberry.driver.find_element_by_xpath(
+                    xpaths['statement_btn']).click()
+                peerberry.wdwait(
+                    EC.text_to_be_present_in_element(
+                        ((By.XPATH, xpaths['start_balance'])),
+                        'Eröffnungssaldo '+str(
+                            self.date_range[0]).format('%Y-%m-%d')))
+            except NoSuchElementException:
+                raise RuntimeError('Generierung des {0}-Kontoauszugs konnte '
+                                   'nicht gestartet werden.'.format(self.name))
+            except TimeoutException:
+                raise RuntimeError('Generierung des {0}-Kontoauszugs hat '
+                                   'zu lange gedauert.'.format(self.name))
 
-    Returns:
-        tuple(pandas.DataFrame, set(str)): tuple with two elements. The first
-        element is the data frame containing the parsed results. The second
-        element is a set containing all unknown cash flow types.
+            peerberry.download_statement(
+                'transactions*.csv', self.statement_file_name,
+                (By.XPATH, xpaths['download_btn']), actions='move_to_element')
 
-    """
-    parser = P2PParser('PeerBerry', date_range, input_file)
+    def parse_statement(self, statement_file_name: str = None) \
+            -> Tuple[pd.DataFrame, str]:
+        """
+        Parser for Peerberry.
 
-    # Create a DataFrame with zero entries if there were no cashflows
-    if parser.df.empty:
-        parser.parse_statement()
-        return (parser.df, '')
+        Keyword Args:
+            statement_file_name: File name including path of the account
+                statement which should be parsed
 
-    # Define mapping between PeerBerry and easyP2P cashflow types and column
-    # names
-    cashflow_types = {
-        'Amount of interest payment received': parser.INTEREST_PAYMENT,
-        'Amount of principal payment received': parser.REDEMPTION_PAYMENT,
-        'Investment': parser.INVESTMENT_PAYMENT}
-    rename_columns = {'Currency Id': parser.CURRENCY, 'Date': parser.DATE}
+        Returns:
+            Tuple with two elements. The first
+            element is the data frame containing the parsed results. The second
+            element is a set containing all unknown cash flow types.
 
-    unknown_cf_types = parser.parse_statement(
-        '%Y-%m-%d', rename_columns, cashflow_types, 'Type', 'Amount')
+        """
+        if statement_file_name is not None:
+            self.statement_file_name = statement_file_name
 
-    return (parser.df, unknown_cf_types)
+        parser = P2PParser(self.name, self.date_range, self.statement_file_name)
+
+        # Create a DataFrame with zero entries if there were no cashflows
+        if parser.df.empty:
+            parser.parse_statement()
+            return (parser.df, '')
+
+        # Define mapping between PeerBerry and easyP2P cashflow types and column
+        # names
+        cashflow_types = {
+            'Amount of interest payment received': parser.INTEREST_PAYMENT,
+            'Amount of principal payment received': parser.REDEMPTION_PAYMENT,
+            'Investment': parser.INVESTMENT_PAYMENT}
+        rename_columns = {'Currency Id': parser.CURRENCY, 'Date': parser.DATE}
+
+        unknown_cf_types = parser.parse_statement(
+            '%Y-%m-%d', rename_columns, cashflow_types, 'Type', 'Amount')
+
+        return (parser.df, unknown_cf_types)
