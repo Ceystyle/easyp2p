822a0e589b52e0550230566605ca1cef45d5738c
Niko Sandschneider <info@ceystyle.de>
2019-04-25 12:17:15 +0200

    p2p_helper_tests: simplify and fix some pylint warnings

--- tests/p2p_helper_tests.py
+++ tests/p2p_helper_tests.py
@@ -11,13 +11,26 @@ results.
 from datetime import date
 import os
 import sys
+from typing import Tuple
 
 import parser_tests
 import easyp2p.platforms as p2p_platforms
 import easyp2p.p2p_helper as p2p_helper
 
 
-def _generate_parser_results(platform, date_range, input_file, output_file):
+def _generate_parser_results(
+        platform: str, date_range: Tuple[date, date], input_file: str,
+        output_file: str) -> None:
+    """
+    Helper function for generating, comparing and writing parser results.
+
+    Args:
+        platform: Name of the P2P platform
+        date_range: Date range of the account statement
+        input_file: File name including path with parser input data
+        output_file: File name including path where results should be saved
+
+    """
     if os.path.isfile(output_file):
         df_old = p2p_helper.get_df_from_file(output_file)
     platform_class = getattr(
@@ -27,15 +40,40 @@ def _generate_parser_results(platform, date_range, input_file, output_file):
     if os.path.isfile(output_file):
         print('New df:\n', df)
         print('Old df:\n', df_old)
-        choice = input('Do you want to replace old with new df (y/n)? ')
-        if choice != 'y':
+        choice = input('Do you want to replace old with new df (y/n/q)? ')
+        if choice.lower() == 'q':
+            sys.exit()
+        elif choice.lower() != 'y':
             return
     else:
         print('Df:\n', df)
-        choice = input('Do you want to save the df (y/n)? ')
-        if choice != 'y':
+        choice = input('Do you want to save the df (y/n/q)? ')
+        if choice.lower() == 'q':
+            sys.exit()
+        elif choice.lower() != 'y':
             return
     df.to_csv(output_file)
+
+
+def _get_choice_from_user(message: str) -> bool:
+    """
+    Helper function to get confirmation from the user.
+
+    Args:
+        message: Message for the user
+
+    Returns:
+        True if user chooses y, False otherwise
+
+    """
+    choice = input(message + ' (y(es)/n(o)/q(uit)? ')
+    if choice.lower() == 'q':
+        sys.exit()
+    elif choice.lower() == 'y':
+        return True
+    else:
+        return False
+
 
 def main():
     """
@@ -49,16 +87,12 @@ def main():
 
     """
     print('WARNING: this will overwrite expected test results!\n')
-    choice = input('Do you want to continue (y/n): ')
-    if choice.lower() != 'y':
-        sys.exit()
 
     platform_list = [
         'Bondora', 'DoFinance', 'Estateguru', 'Grupeer', 'Iuvo', 'Mintos',
-        'PeerBerry', 'Robocash', 'Swaper']
+        'PeerBerry', 'Robocash', 'Swaper', 'Twino']
 
-    choice = input('Generate default parser results (y/n)? ')
-    if choice.lower() == 'y':
+    if _get_choice_from_user('Generate default parser results'):
         for platform in platform_list:
             # DoFinance has its own date range
             if platform == 'DoFinance':
@@ -72,11 +106,11 @@ def main():
             _generate_parser_results(
                 platform, date_range, input_file, output_file)
 
-    choice = input('Generate unknown cashflow parser results (y/n)? ')
-    if choice.lower() == 'y':
+    if _get_choice_from_user('Generate unknown cashflow parser results'):
         for platform in platform_list:
-            if platform in ['Estateguru', 'Mintos', 'Grupeer', 'DoFinance',
-                'Robocash', 'Twino']:
+            if platform in [
+                    'DoFinance', 'Estateguru', 'Grupeer', 'Iuvo',
+                    'Mintos', 'Robocash', 'Twino']:
                 #TODO: generate the unknown cf input files
                 if platform == 'DoFinance':
                     date_range = (date(2018, 5, 1), date(2018, 9, 30))
@@ -90,24 +124,7 @@ def main():
                 _generate_parser_results(
                     platform, date_range, input_file, output_file)
 
-    choice = input('Generate missing month parser results (y/n)? ')
-    if choice.lower() == 'y':
-        for platform in platform_list:
-            # DoFinance has its own date range
-            if platform == 'DoFinance':
-                date_range = (date(2018, 5, 1), date(2018, 9, 30))
-            else:
-                date_range = (date(2018, 8, 1), date(2018, 12, 31))
-            input_file = parser_tests.INPUT_PREFIX \
-                + '{0}_parser_missing_month.{1}'.format(
-                    platform.lower(), parser_tests.PLATFORMS[platform])
-            output_file = parser_tests.RESULT_PREFIX \
-                + '{0}_parser_missing_month.csv'.format(platform.lower())
-            _generate_parser_results(
-                    platform, date_range, input_file, output_file)
-
-    choice = input('Generate no cashflows parser results (y/n)? ')
-    if choice.lower() == 'y':
+    if _get_choice_from_user('Generate no cashflows parser results'):
         for platform in platform_list:
             date_range = (date(2016, 9, 1), date(2016, 12, 31))
             input_file = parser_tests.INPUT_PREFIX \
@@ -116,7 +133,7 @@ def main():
             output_file = parser_tests.RESULT_PREFIX \
                 + '{0}_parser_no_cfs.csv'.format(platform.lower())
             _generate_parser_results(
-                    platform, date_range, input_file, output_file)
+                platform, date_range, input_file, output_file)
 
 
 if __name__ == '__main__':
