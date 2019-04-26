67ec5ac8ce451f4e4173ffe8d45fa8eab3c75f7e
Niko Sandschneider <info@ceystyle.de>
2019-04-26 15:23:20 +0200

    credentials_window: simplify code

--- easyp2p/ui/credentials_window.py
+++ easyp2p/ui/credentials_window.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018-19 Niko Sandschneider
-# pylint: disable=invalid-name
 
 """Module implementing CredentialsWindow."""
 from typing import Optional, Tuple
 
 import keyring
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QDialog, QMessageBox, QWidget
+from PyQt5.QtWidgets import QDialog, QMessageBox
 
 from easyp2p.ui.Ui_credentials_window import Ui_CredentialsWindow
 
 
-# TODO: add methods for changing/deleting credentials
 class CredentialsWindow(QDialog, Ui_CredentialsWindow):
 
     """
@@ -24,98 +21,57 @@ class CredentialsWindow(QDialog, Ui_CredentialsWindow):
 
     """
 
-    def __init__(self, platform: str, parent: QWidget = None) -> None:
+    def __init__(self, platform: str) -> None:
         """
-        Constructor.
+        Constructor of CredentialsWindow.
 
         Args:
             platform: Name of the P2P platform
 
-        Keyword Args:
-            parent: Reference to the parent widget
-
         """
-        super(CredentialsWindow, self).__init__(parent)
+        super().__init__()
         self.setupUi(self)
         self.label_platform.setText('Bitte Benutzername und Passwort für {0} '
-                                    'eingeben:'.format(platform))
-
-    @pyqtSlot()
-    def on_button_box_accepted(self) -> None:
-        """Send accept() signal if OK button is clicked."""
-        self.accept()
-
-    @pyqtSlot()
-    def on_button_box_rejected(self) -> None:
-        """Send reject() signal if Cancel button is clicked."""
-        self.reject()
+            'eingeben:'.format(platform))
 
 
 def get_credentials(platform: str) -> Optional[Tuple[str, str]]:
     """
-    Get credentials for P2P platform from keyring or from user input.
+    Get credentials for P2P platform from keyring.
 
     If a keyring exists, try to get credentials from it. If not or if they
-    cannot be found, ask user for credentials. If the save_in_keyring check box
-    is toggled, the supplied credentials will be saved in the keyring.
+    cannot be found, ask user for credentials.
 
     Args:
         platform: Name of the P2P platform
 
     Returns:
-        Tuple (username, password) on success, None if user clicks Cancel
+        Tuple (username, password) on success, None otherwise
 
     """
-    _done = False
     _ask_for_credentials = False
-    _save_in_keyring = False
 
-    while not _done:
-
-        if keyring.get_keyring():
-            try:
-                username = keyring.get_password(platform, 'username')
-                password = keyring.get_password(platform, username)
-                _done = True
-            except TypeError:
-                # Either username or password were not found in the keyring
-                _ask_for_credentials = True
-        else:
+    if keyring.get_keyring():
+        try:
+            username = keyring.get_password(platform, 'username')
+            password = keyring.get_password(platform, username)
+        except TypeError:
+            # Either username or password were not found in the keyring
             _ask_for_credentials = True
+    else:
+        _ask_for_credentials = True
 
-        if _ask_for_credentials:
-            credentials_window = CredentialsWindow(platform)
-            if not credentials_window.exec_():
-                # User clicked the Cancel button
-                return None
-            username = credentials_window.line_edit_username.text()
-            password = credentials_window.line_edit_password.text()
-
-            if not username or not password:
-                QMessageBox.warning(
-                    credentials_window, 'Felder nicht ausgefüllt', 'Bitte '
-                    'Felder für Benutzername und Passwort ausfüllen!')
-            else:
-                _done = True
-                _save_in_keyring = (
-                    credentials_window.check_box_save_in_keyring.isChecked())
-
-    if _save_in_keyring:
-        if not save_credentials(platform, username, password):
-            QMessageBox.warning(
-                credentials_window, 'Speichern im Keyring fehlgeschlagen!',
-                'Speichern des Passworts im Keyring war leider nicht '
-                'erfolgreich!')
+    if _ask_for_credentials:
+        (username, password) = get_credentials_from_user(platform)
+        if not username or not password:
+            return None
 
     return (username, password)
 
-def save_credentials(platform: str, username: str, password: str) -> bool:
+def save_credentials_in_keyring(
+        platform: str, username: str, password: str) -> bool:
     """
     Save credentials for P2P platform in keyring.
-
-    If a keyring exists, try to save credentials in it. If not or if they
-    cannot be found, ask user for credentials. If the save_in_keyring check box
-    is toggled, the supplied credentials will be saved in the keyring.
 
     Args:
         platform: Name of P2P platform
@@ -135,3 +91,39 @@ def save_credentials(platform: str, username: str, password: str) -> bool:
             return False
 
     return False
+
+def get_credentials_from_user(
+        platform: str) -> Tuple[Optional[str], Optional[str]]:
+    """
+    Ask user for P2P platform credentials.
+
+    Args:
+        platform: Name of the P2P platform
+
+    Returns:
+        Tuple (username, password) on success, (None, None) otherwise
+
+    """
+    username, password = None, None
+    while not username or not password:
+        credentials_window = CredentialsWindow(platform)
+
+        if not credentials_window.exec_():
+            # User clicked the Cancel button
+            return (None, None)
+
+        username = credentials_window.line_edit_username.text()
+        password = credentials_window.line_edit_password.text()
+        if not username or not password:
+            QMessageBox.warning(
+                credentials_window, 'Felder nicht ausgefüllt', 'Bitte '
+                'Felder für Benutzername und Passwort ausfüllen!')
+
+    if credentials_window.check_box_save_in_keyring.isChecked():
+        if not save_credentials_in_keyring(platform, username, password):
+            QMessageBox.warning(
+                credentials_window, 'Speichern im Keyring fehlgeschlagen!',
+                'Speichern des Passworts im Keyring war leider nicht '
+                'erfolgreich!')
+
+    return (username, password)
