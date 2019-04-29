ceae19d209491a08ecf645ee1ec20a461a4aa4a3
Niko Sandschneider <info@ceystyle.de>
2019-04-29 14:21:49 +0200

    Add module to keep track of all settings

---
+++ easyp2p/p2p_settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Niko Sandschneider
+
+"""Module containing all configurable settings for easyp2p."""
+
+class Settings():
+    
+    """A class to store all settings of easyp2p."""
+    
+    def __init__(self):
+        self.platforms = set()
+        self.date_range = None
+        self.output_file = None
