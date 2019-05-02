9c50623cec0edacc10333bd6f1348f22857638af
Niko Sandschneider <info@ceystyle.de>
2019-05-02 18:55:29 +0200

    Simplify the tests for CredentialsWindow

--- tests/test_credentials_window.py
+++ tests/test_credentials_window.py
@@ -5,7 +5,6 @@
 
 import functools
 import sys
-from typing import Union
 import unittest
 
 from PyQt5.QtCore import QTimer, Qt
@@ -14,8 +13,6 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtTest import QTest
 
 from easyp2p.ui.credentials_window import CredentialsWindow
-from easyp2p.ui.progress_window import ProgressWindow
-from easyp2p.ui.settings_window import SettingsWindow
 
 app = QApplication(sys.argv)
 
@@ -23,11 +20,6 @@ app = QApplication(sys.argv)
 class CredentialsWindowTests(unittest.TestCase):
 
     """Test the credentials window of easyp2p."""
-
-    def setUp(self):
-        """Initialize CredentialsWindow."""
-        self.message_box_open = False
-        self.window_open = False
 
     def test_defaults_with_keyring(self):
         """Test default behaviour if keyring_exists==True."""
@@ -59,36 +51,31 @@ class CredentialsWindowTests(unittest.TestCase):
     def test_no_input(self):
         """Test clicking OK without entering credentials."""
         self.form = CredentialsWindow('TestPlatform', True)
-        QTimer.singleShot(500, self.is_message_box_open)
-        QTimer.singleShot(
-            500, functools.partial(self.is_window_open, CredentialsWindow))
-        # Check that message box opened and CredentialsWindow is still open
+        self.form.setVisible(True)
+        self.assertTrue(self.form.isVisible())
+        # Make sure a QMessageBox appears if no credentials are provided
+        QTimer.singleShot(100, functools.partial(accept_qmessagebox, self))
         self.form.button_box.button(QDialogButtonBox.Ok).click()
-        self.assertTrue(self.message_box_open)
+        # Credentials window must still be visible
+        self.assertTrue(self.form.isVisible())
+        self.form.button_box.button(QDialogButtonBox.Cancel).click()
+        self.assertFalse(self.form.isVisible())
 
-    def is_message_box_open(self) -> bool:
-        """Helper method to determine if a QMessageBox is open."""
-        all_top_level_widgets = QApplication.topLevelWidgets()
-        for widget in all_top_level_widgets:
-            if isinstance(widget, QMessageBox):
-                QTest.keyClick(widget, Qt.Key_Enter)
-                self.message_box_open = True
-                return True
-        self.message_box_open = False
-        return False
 
-    def is_window_open(
-            self, window: Union[
-                CredentialsWindow, ProgressWindow, SettingsWindow]) -> bool:
-        """Helper method to determine if a window is open."""
-        all_top_level_widgets = QApplication.topLevelWidgets()
-        for widget in all_top_level_widgets:
-            if isinstance(widget, window):
-                widget.reject()
-                self.window_open = True
-                return True
-        self.window_open = False
-        return False
+def accept_qmessagebox(testclass: unittest.TestCase) -> None:
+    """
+    Check if a QMessageBox is open. If yes accept it. If no fail the test.
+
+    Args:
+        testclass: Instance of unittest.TestCase
+
+    """
+    all_top_level_widgets = QApplication.topLevelWidgets()
+    for widget in all_top_level_widgets:
+        if isinstance(widget, QMessageBox):
+            QTest.keyClick(widget, Qt.Key_Enter)
+            return
+    testclass.fail('QMessageBox did not open!')
 
 
 if __name__ == "__main__":
