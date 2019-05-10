813ea3950a5ac9636957cc64aa49281f90f1a3ec
Niko Sandschneider <info@ceystyle.de>
2019-05-10 11:45:39 +0200

    Add more tests for credentials_window and simplify existing ones

--- tests/test_credentials_window.py
+++ tests/test_credentials_window.py
@@ -3,15 +3,12 @@
 
 """Module containing all tests for the credentials window of easyp2p."""
 
-import functools
 import sys
-import unittest
+import unittest.mock
 
-from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QDialogButtonBox, QLineEdit
 
 from easyp2p.ui.credentials_window import CredentialsWindow
-import tests.utils as utils
 
 APP = QApplication(sys.argv)
 
@@ -23,12 +20,14 @@ class CredentialsWindowTests(unittest.TestCase):
     def setUp(self):
         """Initialize dialog for most of the tests."""
         self.form = CredentialsWindow('TestPlatform', True)
-        self.test_results = []
 
     def test_defaults_with_keyring(self):
         """Test default behaviour if keyring_exists==True."""
         self.assertFalse(self.form.line_edit_username.text())
         self.assertFalse(self.form.line_edit_password.text())
+        self.assertEqual(
+            self.form.label_platform.text(),
+            'Bitte Benutzername und Passwort für TestPlatform eingeben:')
         self.assertFalse(self.form.check_box_save_in_keyring.isChecked())
 
     def test_defaults_without_keyring(self):
@@ -36,6 +35,9 @@ class CredentialsWindowTests(unittest.TestCase):
         self.form = CredentialsWindow('TestPlatform', False)
         self.assertFalse(self.form.line_edit_username.text())
         self.assertFalse(self.form.line_edit_password.text())
+        self.assertEqual(
+            self.form.label_platform.text(),
+            'Bitte Benutzername und Passwort für TestPlatform eingeben:')
         self.assertFalse(self.form.check_box_save_in_keyring.isChecked())
         self.assertFalse(self.form.check_box_save_in_keyring.isEnabled())
 
@@ -51,48 +53,88 @@ class CredentialsWindowTests(unittest.TestCase):
         self.assertFalse(self.form.check_box_save_in_keyring.isChecked())
         self.assertFalse(self.form.check_box_save_in_keyring.isEnabled())
 
-    def test_no_input(self):
+    @unittest.mock.patch('easyp2p.ui.credentials_window.QMessageBox.warning')
+    def test_no_input(self, mock_warning):
         """Test clicking OK without entering credentials."""
-        QTimer.singleShot(
-            0, self.form.button_box.button(QDialogButtonBox.Ok).click)
-        QTimer.singleShot(
-            400, functools.partial(utils.accept_qmessagebox, self))
-        QTimer.singleShot(
-            500, functools.partial(
-                utils.window_visible, self, CredentialsWindow))
-        QTimer.singleShot(
-            600, self.form.button_box.button(QDialogButtonBox.Cancel).click)
-        self.form.exec_()
-        expected_results = [utils.QMSG_BOX_OPEN, utils.CRED_WINDOW_VISIBLE]
-        self.assertEqual(self.test_results, expected_results)
-        self.assertFalse(self.form.isVisible())
+        self.form.button_box.button(self.form.button_box.Ok).click()
+        # Check that a warning was emitted and the window is still open
+        mock_warning.assert_called_once_with(
+            self.form, 'Felder nicht ausgefüllt',
+            'Bitte Felder für Benutzername und Passwort ausfüllen!')
+        self.assertFalse(self.form.result())
+        self.assertIsNone(self.form.username)
+        self.assertIsNone(self.form.password)
+        self.assertFalse(self.form.save_in_keyring)
 
-    def test_input_credentials(self):
-        """Test entering credentials."""
+    @unittest.mock.patch('easyp2p.ui.credentials_window.QMessageBox.warning')
+    def test_missing_username(self, mock_warning):
+        """Test clicking OK without entering username."""
+        QLineEdit.setText(self.form.line_edit_password, 'TestPass')
+        self.form.button_box.button(self.form.button_box.Ok).click()
+        # Check that a warning was emitted and the window is still open
+        mock_warning.assert_called_once_with(
+            self.form, 'Felder nicht ausgefüllt',
+            'Bitte Felder für Benutzername und Passwort ausfüllen!')
+        self.assertFalse(self.form.result())
+        self.assertIsNone(self.form.username)
+        self.assertIsNone(self.form.password)
+        self.assertFalse(self.form.save_in_keyring)
+
+    @unittest.mock.patch('easyp2p.ui.credentials_window.QMessageBox.warning')
+    def test_missing_password(self, mock_warning):
+        """Test clicking OK without entering password."""
+        QLineEdit.setText(self.form.line_edit_username, 'TestUser')
+        self.form.button_box.button(self.form.button_box.Ok).click()
+        # Check that a warning was emitted and the window is still open
+        mock_warning.assert_called_once_with(
+            self.form, 'Felder nicht ausgefüllt',
+            'Bitte Felder für Benutzername und Passwort ausfüllen!')
+        self.assertFalse(self.form.result())
+        self.assertIsNone(self.form.username)
+        self.assertIsNone(self.form.password)
+        self.assertFalse(self.form.save_in_keyring)
+
+    def test_input_credentials_save_in_keyring_true(self):
+        """
+        Test entering credentials and check save_in_keyring then click OK.
+        """
+        QLineEdit.setText(self.form.line_edit_username, 'TestUser')
+        QLineEdit.setText(self.form.line_edit_password, 'TestPass')
+        self.form.check_box_save_in_keyring.setChecked(True)
+        self.form.button_box.button(QDialogButtonBox.Ok).click()
+        self.assertEqual(self.form.username, 'TestUser')
+        self.assertEqual(self.form.password, 'TestPass')
+        self.assertTrue(self.form.save_in_keyring)
+
+    def test_input_credentials_save_in_keyring_false(self):
+        """
+        Test entering credentials and not check save_in_keyring then click OK.
+        """
         QLineEdit.setText(self.form.line_edit_username, 'TestUser')
         QLineEdit.setText(self.form.line_edit_password, 'TestPass')
         self.form.button_box.button(QDialogButtonBox.Ok).click()
         self.assertEqual(self.form.username, 'TestUser')
         self.assertEqual(self.form.password, 'TestPass')
+        self.assertFalse(self.form.save_in_keyring)
 
     def test_input_credentials_cancel(self):
-        """Test entering credentials and then clicking Cancel."""
+        """Test enter credentials, check save_in_keyring and click Cancel."""
         QLineEdit.setText(self.form.line_edit_username, 'TestUser')
         QLineEdit.setText(self.form.line_edit_password, 'TestPass')
-        self.form.button_box.button(QDialogButtonBox.Cancel).click()
-        self.assertIsNone(self.form.username)
-        self.assertIsNone(self.form.password)
-
-    def test_input_credentials_cancel_with_save_in_keyring(self):
-        """
-        Test cancel without entering credentials and save_in_keyring checked.
-        """
         self.form.check_box_save_in_keyring.setChecked(True)
         self.form.button_box.button(QDialogButtonBox.Cancel).click()
         self.assertIsNone(self.form.username)
         self.assertIsNone(self.form.password)
         self.assertFalse(self.form.save_in_keyring)
 
+    @unittest.mock.patch('easyp2p.ui.credentials_window.QMessageBox.warning')
+    def test_warn_user(self, mock_warning):
+        self.form.warn_user('TestWarning', 'This is a test warning!')
+        mock_warning.assert_called_once_with(
+            self.form, 'TestWarning', 'This is a test warning!')
+
 
 if __name__ == "__main__":
-    unittest.main()
+    runner = unittest.TextTestRunner(verbosity=3)
+    suite = unittest.TestLoader().loadTestsFromTestCase(CredentialsWindowTests)
+    result = runner.run(suite)
