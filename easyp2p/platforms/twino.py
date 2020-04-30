b5ec64942c477d3748b4e88e6e759a114458f538
Niko Sandschneider <info@ceystyle.de>
2020-04-30 12:06:27 +0200

    Twino: update login button locator

--- easyp2p/platforms/twino.py
+++ easyp2p/platforms/twino.py
@@ -66,9 +66,6 @@ class Twino:
         }
         xpaths = {
             'end_date': '//*[@date-picker="filterData.processingDateTo"]',
-            'login_btn': (
-                '/html/body/div[1]/div[2]/div[1]/header[1]/div/nav/div/div[1]'
-                '/button'),
             'logout_btn': '//a[@href="/logout"]',
             'start_date': '//*[@date-picker="filterData.processingDateFrom"]',
             'statement': (
@@ -78,14 +75,14 @@ class Twino:
 
         with P2PPlatform(
                 self.name, driver, urls,
-                EC.element_to_be_clickable((By.XPATH, xpaths['login_btn'])),
+                EC.element_to_be_clickable((By.CLASS_NAME, 'js-login-btn')),
                 logout_locator=(By.XPATH, xpaths['logout_btn']),
                 signals=self.signals) as twino:
 
             twino.log_into_page(
                 'email', 'login-password', credentials,
                 EC.element_to_be_clickable((By.XPATH, xpaths['statement'])),
-                login_locator=(By.XPATH, xpaths['login_btn']))
+                login_locator=(By.CLASS_NAME, 'js-login-btn'))
 
             twino.open_account_statement_page((By.XPATH, xpaths['start_date']))
 
@@ -139,6 +136,7 @@ class Twino:
             'BUY_SHARES PRINCIPAL': parser.INVESTMENT_PAYMENT,
             'CURRENCY_FLUCTUATION INTEREST': parser.INTEREST_PAYMENT,
             'EXTENSION INTEREST': parser.INTEREST_PAYMENT,
+            'EXTENSION PRINCIPAL': parser.REDEMPTION_PAYMENT,
             'REPAYMENT INTEREST': parser.INTEREST_PAYMENT,
             'REPAYMENT PRINCIPAL': parser.REDEMPTION_PAYMENT,
             'REPURCHASE INTEREST': parser.BUYBACK_INTEREST_PAYMENT,
