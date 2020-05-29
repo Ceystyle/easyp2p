5809f9825c20168123397c7b14add3a153712a40
Niko Sandschneider <info@ceystyle.de>
2020-05-29 19:02:02 +0200

    easyp2p: add docstring to __init__.py

--- easyp2p/__init__.py
+++ easyp2p/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2018-2020 Niko Sandschneider
 
+"""
+easyp2p is an application for downloading and aggregating account statements
+from several people-to-people (P2P) lending platforms.
+"""
+
 __version__ = '0.0.1'
